eb0c593dc9d02edc2a2ab51483a1263494c3f25e
babel <cholodecki@gmail.com>
2023-12-03 15:02:07 +0100

    Initial Commit

---
+++ popPhysicsSim/popPhysicsSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  popPhysicsSimApp.swift
+//  popPhysicsSim
+//
+//  Created by babel on 03/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct popPhysicsSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
